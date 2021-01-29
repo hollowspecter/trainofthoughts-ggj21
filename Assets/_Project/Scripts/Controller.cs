299303465465442e45ad7e68a776fa2e854c1b0c
Vivien Baguio <baguiov@posteo.net>
2021-01-29 21:04:27 +0100

    prepare dotween

---
+++ Assets/_Project/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	private new Rigidbody2D rigidbody;
+	private float input;
+
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		HandleInput();
+	}
+
+	private void FixedUpdate()
+	{
+
+	}
+
+	void HandleInput()
+	{
+		input = Input.GetAxisRaw("Vertical");
+	}
+}
