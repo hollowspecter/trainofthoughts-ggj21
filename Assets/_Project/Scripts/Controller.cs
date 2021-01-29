cd8b57a5ae97d06ab6dd59f253d1be1136c9c1d1
Vivien Baguio <baguiov@posteo.net>
2021-01-29 21:15:12 +0100

    rename dotween folder

--- Assets/_Project/Scripts/Controller.cs
+++ Assets/_Project/Scripts/Controller.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+	[SerializeField]
+	private Transform groundCheckOrigin;
+	[SerializeField]
+	private LayerMask groundLayer;
+	[SerializeField]
+	private float jumpForce = 3f;
+	[SerializeField]
+	private float speed = 3f;
+
 	private new Rigidbody2D rigidbody;
 	private float input;
+	private bool isGrounded = false;
 
 	private void Awake()
 	{
@@ -19,7 +29,26 @@ public class Controller : MonoBehaviour
 
 	private void FixedUpdate()
 	{
+		GroundCheck();
+		HandleMovement();
+	}
 
+	void HandleMovement()
+	{
+		Vector2 velocity = new Vector2(speed, rigidbody.velocity.y);
+	}
+
+	void GroundCheck()
+	{
+		var hit = Physics2D.Raycast(groundCheckOrigin.position, Vector2.down, 0.2f, groundLayer);
+		if (hit.collider != null)
+		{
+			isGrounded = true;
+		}
+		else
+		{
+			isGrounded = false;
+		}
 	}
 
 	void HandleInput()
