a0712502728c7ea2a666601882e67aaea5b96b74
Vivien Baguio <baguiov@posteo.net>
2021-01-30 11:51:30 +0100

    Update

---
+++ Assets/_Project/Scripts/Dialogue/ContinueButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContinueButton : MonoBehaviour
+{
+	UnityEngine.UI.Button button;
+
+	private void Awake()
+	{
+		button = GetComponent<UnityEngine.UI.Button>();
+
+
+	}
+}
