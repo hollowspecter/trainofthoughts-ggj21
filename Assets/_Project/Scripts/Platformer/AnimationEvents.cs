8e292fd3195f4f918d006b58c15926ea1a7473c0
Vivien Baguio <baguiov@posteo.net>
2021-01-30 15:45:54 +0100

    adding run cycle + footstep sounds to player

---
+++ Assets/_Project/Scripts/Platformer/AnimationEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour
+{
+	[FMODUnity.EventRef]
+	public string footstepSound;
+
+	public void Footstep()
+	{
+		FMODUnity.RuntimeManager.PlayOneShot(footstepSound);
+	}
+}
