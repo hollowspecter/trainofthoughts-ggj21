630b1429cd15b5ddb4b9f06769d749b48cf33d48
Vivien Baguio <baguiov@posteo.net>
2021-01-30 13:20:38 +0100

    Breaking platform added

---
+++ Assets/_Project/Scripts/Platformer/BreakingPlatform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+
+public class BreakingPlatform : MonoBehaviour
+{
+	[SerializeField]
+	float timeUntilBreak;
+	[SerializeField]
+	Transform shakable;
+
+	public UnityEvent onBreak;
+
+	bool isBreaking = false;
+	new Rigidbody2D rigidbody2D;
+	new Collider2D collider;
+
+	private void Awake()
+	{
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		rigidbody2D.isKinematic = true;
+		collider = GetComponent<Collider2D>();
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (!isBreaking && collision.collider.CompareTag("Player"))
+		{
+			isBreaking = true;
+			StartCoroutine(Break());
+		}
+	}
+
+	private IEnumerator Break()
+	{
+		shakable.DOShakeRotation(timeUntilBreak, 20f, 10, 90, false);
+		yield return new WaitForSeconds(timeUntilBreak);
+		collider.enabled = false;
+		onBreak?.Invoke();
+		shakable.gameObject.SetActive(false);
+		yield return new WaitForSeconds(10f);
+
+	}
+}
