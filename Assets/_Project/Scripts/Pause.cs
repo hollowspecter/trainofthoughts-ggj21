9e21f0663671c4a32a8359bf31931f3c06de38bc
Vivien Baguio <baguiov@posteo.net>
2021-01-31 14:13:17 +0100

    pause is built in

---
+++ Assets/_Project/Scripts/Pause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+	[FMODUnity.EventRef]
+	public string pauseSound;
+	public FMODUnity.StudioEventEmitter pauseSnapshot;
+
+	private bool paused = false;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			paused = !paused;
+			TogglePause(paused);
+		}
+	}
+
+	private void TogglePause(bool isPaused)
+	{
+		if (isPaused)
+		{
+			Time.timeScale = 0f;
+			FMODUnity.RuntimeManager.PlayOneShot(pauseSound);
+			pauseSnapshot.Play();
+		}
+		else
+		{
+			Time.timeScale = 1f;
+			FMODUnity.RuntimeManager.PlayOneShot(pauseSound);
+			pauseSnapshot.Stop();
+		}
+	}
+}
